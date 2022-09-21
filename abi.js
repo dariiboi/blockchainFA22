7ae29cead2e03af2c4cff0e7d8d931474c2090b3
darius <darius@dari.us>
2022-09-21 18:06:36 -0500

    pics added

---
+++ abi.js
@@ -0,0 +1,39 @@
+// change this each time you deploy a new contract
+var contractAddress = "0xD181268E7a1b5D3DFA6504A63aAcF6328CD32c0F"
+
+// paste contract ABI here
+var contractABI = [
+	{
+		"inputs": [],
+		"name": "changeStatus",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getStatus",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "isArt",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
